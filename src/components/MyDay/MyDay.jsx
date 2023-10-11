5b8f27c13aecabcc3334e6cb5f6709708e00785e
Angelica Beltran <angelica.beltran11@gmail.com>
2023-10-11 13:15:05 -0500

    working on saving stuff to the store

--- src/components/MyDay/MyDay.jsx
+++ src/components/MyDay/MyDay.jsx
@@ -8,7 +8,12 @@ import {
   Toolbar,
   TextField,
   Typography,
+  Card,
+  Grid,
+  CardActionArea,
+  CardContent,
 } from "@mui/material";
+
 // import "./MyDay.css";
 import { useHistory } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
@@ -19,10 +24,10 @@ export default function MyDay() {
   const dispatch = useDispatch();
   const history = useHistory();
   const taskStore = useSelector((store) => store.taskStore);
-
+  console.log('this is the taskStore', taskStore);
+  
   useEffect(() => {
     dispatch({ type: "GET_TASKS" });
-
   }, []);
 
   function AddTaskOnClick() {
@@ -30,18 +35,48 @@ export default function MyDay() {
     history.push("/AddTask");
   }
   return (
-    <AppBar position="static">
-      <Toolbar
-        sx={{
-          display: "flex",
-          justifyContent: "space-between",
-        }}
-      >
-        <Button variant="contained">Today</Button>
-        <Button variant="contained" onClick={AddTaskOnClick}>
-          Add Task
-        </Button>
-      </Toolbar>
-    </AppBar>
+    <>
+      <AppBar position="static">
+        <Toolbar
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <Button variant="contained">Today</Button>
+          <Button variant="contained" onClick={AddTaskOnClick}>
+            Add Task
+          </Button>
+        </Toolbar>
+      </AppBar>
+
+      <Box style={{ marginBottom: "90px", flexGrow: 1 }}>
+        <section className="tasksClass">
+          <Grid>
+            {/* container spacing={8} marginTop="10px" marginBottom="80px" */}
+            {/* {taskStore.map((task) => ( */}
+              <Grid>
+                {/* item xs={12} sm={6} md={4} lg={3} key={movie.id} */}
+                <Card>
+                  {/* sx={{ boxShadow: "5px 10px #f5f5f5" }} */}
+                  <CardActionArea>
+                    {/* key={movie.id} */}
+                    <CardContent>
+                      <Typography
+                      // fontFamily="Reem Kufi"
+                      // color="#2c698d"
+                      // variant="h4"
+                      >
+                        {/* {movie.title} */}
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+              </Grid>
+            {/* ))} */}
+          </Grid>
+        </section>
+      </Box>
+    </>
   );
 }
