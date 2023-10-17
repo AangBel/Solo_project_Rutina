91781ab97de3cda3259896868e62335316262ffc
Angelica Beltran <angelica.beltran11@gmail.com>
2023-10-17 00:31:12 -0500

    css on user page

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import LogOutButton from "../LogOutButton/LogOutButton";
 import { useSelector } from "react-redux";
 
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
 
-import './UserPage.css'
-import framedBoat from '../images/framedBoat.gif'
-import CatInABoat from '../images/CatInABoat.png';
-import bubble from '../images/bubble.png';
+import "./UserPage.css";
+import framedBoat from "../images/framedBoat.gif";
+import CatInABoat from "../images/CatInABoat.png";
+import bubble from "../images/bubble.png";
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
@@ -17,19 +17,16 @@ function UserPage() {
   return (
     <>
       <div className="container">
-      <div className="card shadow">
+        <div className="card shadow">
+          <h2 className="welcomeText">Welcome, {user.username}!</h2>
+          {/* <img src={framedBoat} alt="Boat bobbing amongst waves gif" /> */}
+          <img src={bubble} alt="a speech bubble" className="bubble" />
+          <img src={CatInABoat} alt="Boat bobbing amongst waves gif" />
 
-        <h2 className="welcomeText">Welcome, {user.username}!</h2>
-      {/* <img src={framedBoat} alt="Boat bobbing amongst waves gif" /> */}
-      <img src={bubble} alt="a speech bubble" className="bubble"/>
-      <img src={CatInABoat} alt="Boat bobbing amongst waves gif" />
-
-
-        {/* <p className="idText">Your ID is: {user.id}</p> */}
-        <LogOutButton className="btn" />
+          {/* <p className="idText">Your ID is: {user.id}</p> */}
+          <LogOutButton className="btn" />
+        </div>
       </div>
-      </div>
-      
     </>
   );
 }
