4c2bae76635dafb9c8cc6bc30f1eda0e3e2d460e
Angelica Beltran <angelica.beltran11@gmail.com>
2023-10-17 00:25:14 -0500

    css and pomodoro

--- src/components/Pomodoro/Pomodoro.jsx
+++ src/components/Pomodoro/Pomodoro.jsx
@@ -1,58 +1,73 @@
 import React, { useState, useEffect } from "react";
+import cuteTomato from "../images/cuteTomato.png";
+import "./Pomodoro.css";
 
 const Pomodoro = () => {
-    const [timer, setTimer] = useState(25 * 60); // 25 minutes in seconds
-    const [isActive, setIsActive] = useState(false);
-    const [isBreak, setIsBreak] = useState(false);
+  const [timer, setTimer] = useState(25 * 60);
+  const [isActive, setIsActive] = useState(false);
+  const [isBreak, setIsBreak] = useState(false);
 
-    useEffect(() => {
-        let interval = null;
+  useEffect(() => {
+    let interval = null;
 
-        if (isActive && timer > 0) {
-            interval = setInterval(() => {
-                setTimer(timer => timer - 1);
-            }, 1000);
-        } else if (isActive && timer === 0) {
-            setIsActive(false);
-            setIsBreak(prevState => !prevState);
-            setTimer(isBreak ? 5 * 60 : 25 * 60); // 5 minutes break or 25 minutes work
-        } else {
-            clearInterval(interval);
-        }
+    if (isActive && timer > 0) {
+      interval = setInterval(() => {
+        setTimer((timer) => timer - 1);
+      }, 1000);
+    } else if (isActive && timer === 0) {
+      setIsActive(false);
+      setIsBreak((prevState) => !prevState);
+      setTimer(isBreak ? 5 * 60 : 25 * 60);
+    } else {
+      clearInterval(interval);
+    }
 
-        return () => clearInterval(interval);
-    }, [isActive, timer, isBreak]);
+    return () => clearInterval(interval);
+  }, [isActive, timer, isBreak]);
 
-    const handleStart = () => {
-        setIsActive(true);
-    };
+  const handleStart = () => {
+    setIsActive(true);
+  };
 
-    const handleReset = () => {
-        setIsActive(false);
-        setIsBreak(false);
-        setTimer(25 * 60);
-    };
+  const handleReset = () => {
+    setIsActive(false);
+    setIsBreak(false);
+    setTimer(25 * 60);
+  };
 
-    const formatTime = time => {
-        const minutes = Math.floor(time / 60)
-            .toString()
-            .padStart(2, "0");
-        const seconds = (time % 60).toString().padStart(2, "0");
-        return `${minutes}:${seconds}`;
-    };
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60)
+      .toString()
+      .padStart(2, "0");
+    const seconds = (time % 60).toString().padStart(2, "0");
+    return `${minutes}:${seconds}`;
+  };
 
-    return (
-        <div>
+  return (
+    <>
+      <div className="squishCardClass">
+        <div className="tomatoCard">
+          <div className="card-container">
+            <img src={cuteTomato} className="tomato" alt="Tomato" />
             <h1>{isBreak ? "Break" : "Work"}</h1>
             <h2>{formatTime(timer)}</h2>
-            {!isActive && (
-                <button onClick={handleStart}>Start</button>
-            )}
-            {isActive && (
-                <button onClick={handleReset}>Reset</button>
-            )}
+            <div className="button-container">
+              {!isActive && (
+                <button onClick={handleStart} className="learn-more">
+                  Start
+                </button>
+              )}
+              {isActive && (
+                <button onClick={handleReset} className="learn-more">
+                  Reset
+                </button>
+              )}
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </>
+  );
 };
 
 export default Pomodoro;
